5d9cdbccf0fc1009cf81c9b9a3a89ef98ffdb76b
= <=>
2024-02-23 14:49:10 +0100

    chore: location of content folder

--- src/app/_utils/fetchContent.ts
+++ src/app/_utils/fetchContent.ts
@@ -1,6 +1,7 @@
 import { promises as fs } from "fs";
+import path from "path";
 
 export const fetchContent = (contentFileName: string) => 
     fs
-        .readFile(`${process.env.contentFolderPath}/${contentFileName}`, "utf-8")
+        .readFile(`${path.resolve("src/app/content")}/${contentFileName}`, "utf-8")
         .then((value) => JSON.parse(value));
